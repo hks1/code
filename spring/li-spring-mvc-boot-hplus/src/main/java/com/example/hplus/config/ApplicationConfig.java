bcd7611353e300548a3c3c2dc242445b4cd595cc
hks1 <hks1@users.noreply.github.com>
2022-10-14 22:39:10 -0400

    async request processing - return Callable in Controller

--- spring/li-spring-mvc-boot-hplus/src/main/java/com/example/hplus/config/ApplicationConfig.java
+++ spring/li-spring-mvc-boot-hplus/src/main/java/com/example/hplus/config/ApplicationConfig.java
@@ -4,7 +4,10 @@ import com.example.hplus.convertors.StringToEnumConvertor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -31,5 +34,18 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToEnumConvertor());
+    }
+
+    @Override
+    protected void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setDefaultTimeout(5000);
+        configurer.setTaskExecutor(mvcTaskExecutor());
+    }
+
+    @Bean
+    public AsyncTaskExecutor mvcTaskExecutor(){
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("hplusapp-thread-");
+        return threadPoolTaskExecutor;
     }
 }
--- spring/li-spring-mvc-boot-hplus/src/main/java/com/example/hplus/controllers/SearchController.java
+++ spring/li-spring-mvc-boot-hplus/src/main/java/com/example/hplus/controllers/SearchController.java
@@ -3,13 +3,16 @@ package com.example.hplus.controllers;
 import com.example.hplus.beans.Product;
 import com.example.hplus.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 @Controller
 public class SearchController {
@@ -18,13 +21,22 @@ public class SearchController {
     private ProductRepository productRepository;
 
     @GetMapping("/search")
-    public String search(@RequestParam("search") String search, Model model){
+    public Callable<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request){
         System.out.println("in search controller!!!");
         System.out.println("search criteria: " + search);
-        List<Product> products = new ArrayList<>();
-        products = productRepository.searchByName(search);
-        products.stream().forEach(product -> System.out.println(product));
-        model.addAttribute("products", products);
-        return "search";
+        System.out.println("Async supported in application: " + request.isAsyncSupported());
+        System.out.println("Thread from the servlet container: " + Thread.currentThread().getName());
+
+        return () -> {
+            // to simulate the delay
+            Thread.sleep(3000);
+            System.out.println("Thread from the spring mvc task executor: " + Thread.currentThread().getName());
+            List<Product> products = new ArrayList<>();
+            products = productRepository.searchByName(search);
+            products.stream().forEach(product -> System.out.println(product));
+            model.addAttribute("products", products);
+            return "search";
+        };
+
     }
 }
