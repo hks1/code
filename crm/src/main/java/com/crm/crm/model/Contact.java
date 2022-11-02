987e53735038b10240eca3394dbf2431a390a577
hks1 <hks1@users.noreply.github.com>
2022-11-02 14:45:46 -0400

    spring models and http with spring - backend ready

--- crm/src/main/java/com/crm/crm/model/Contact.java
+++ crm/src/main/java/com/crm/crm/model/Contact.java
@@ -1,2 +1,23 @@
-package com.crm.crm.model;public class Contact {
+package com.crm.crm.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Contact {
+    private @Id @GeneratedValue Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private Contact(){}
+
+    public Contact(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
---
+++ crm/src/main/java/com/crm/crm/model/ContactRepository.java
@@ -0,0 +1,6 @@
+package com.crm.crm.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContactRepository extends CrudRepository<Contact, Long>{
+}
