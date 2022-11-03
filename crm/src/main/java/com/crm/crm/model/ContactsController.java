51c489ce93c95f3a8e1c3c663b6be35aab2e3a04
hks1 <hks1@users.noreply.github.com>
2022-11-02 21:43:02 -0400

    add server controller for CORS

--- crm/src/main/java/com/crm/crm/model/ContactsController.java
+++ crm/src/main/java/com/crm/crm/model/ContactsController.java
@@ -1,2 +1,31 @@
-package com.crm.crm.model;public class ContactsController {
+package com.crm.crm.model;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class ContactsController {
+    private ContactRepository contactRepository;
+
+    public ContactsController(ContactRepository contactRepository){
+        this.contactRepository = contactRepository;
+    }
+
+    @GetMapping("/contacts")
+    Collection<Contact> contacts(){
+        return (Collection<Contact>) contactRepository.findAll();
+    }
+
+    @PostMapping("/contacts")
+    ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact) throws URISyntaxException{
+        Contact result = contactRepository.save(contact);
+        return ResponseEntity.ok().body(result);
+    }
 }
